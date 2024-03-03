c254f569e0ea7c18f150090ae90ad90451da2cb0
/FS6 <support@fs6.club>
2024-03-04 02:16:44 +0300

    v0.2

--- update-repo.sh
+++ update-repo.sh
@@ -1,4 +1,3 @@
-rm -rf Packages*
-dpkg-scanpackages -m ./debs > Packages
-bzip2 Packages
-echo "アップデートが完了しました"
+rm -rf /Users/fs6/Documents/GitHub/repo/Packages.bz2
+bzip2 /Users/fs6/Documents/GitHub/repo/Packages
+echo "Done By FS6"
